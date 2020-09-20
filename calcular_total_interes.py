9b4ccfaa10e4fa0b43b134d590a50a0fa9d0968c
Tita <fioreoyanedelsartori@gmail.com>
2020-09-20 16:14:33 -0300

    actualizacion de formato con dash_

--- calcular_total_interes.py
+++ calcular_total_interes.py
@@ -1,6 +1,6 @@
 def calcular_total():
     ahorros = int(input('Ingresar capital inicial \n'))
-    medida = str(input('Ingresar medida de tiempo (mes, año, etc.)\n'))
+    medida = str(input('Elegir [mes/año]\n'))
     if medida == 'año':
         interes = 'anual'
     elif medida == 'Año':
@@ -18,19 +18,22 @@ def calcular_total():
     tasa_interes = float(input('Ingresar la tasa de interés %s en decimales \n' % interes))
     tiempo = int(input('Ingresar cantidad de %s \n' % medida_pl))
     dash =  '=' * 85
+    dash_ = '-' * 85
     print('Se muestran los valores correspondientes a cada %s' % medida)
     for x in range(0, (tiempo+1)): 
         if x == 0:
             print(dash)
             print(' {:^10s} {:>25s} {:>25s} {:>20s} ' .format(medida.upper(), 'INICIAL', 'INTERÉS', 'TOTAL'))
             print(dash)
+        elif x < 10:
+            interés = ahorros * tasa_interes
+            ahorros = ahorros + interés
+            inicial = ahorros - interés
+            print(' {:^10d} {:>25} {:>25} {:>20} ' .format(x, inicial, interés, ahorros))
+            print(dash_)
         else:
             interés = ahorros * tasa_interes
             ahorros = ahorros + interés
             inicial = ahorros - interés
             print(' {:^10d} {:>25} {:>25} {:>20} ' .format(x, inicial, interés, ahorros))
-    print(dash)            
-
-
-
-
+            print(dash) 
